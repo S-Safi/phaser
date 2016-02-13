5d83ceb1a54958dcfe76fb516d463f21699e5c00
Salam Almajdi <salamalmajdi@gmail.com>
2016-02-14 09:29:20 +1100

    Added arcade physics.

--- easy-game/app/app.js
+++ easy-game/app/app.js
@@ -9,17 +9,54 @@ var game = new Phaser.Game(
 function preload() {
   game.load.image('level1Background', 'assets/levels/test.png');
   game.load.image('player', 'assets/player/derp-ssundee.png');
+  game.load.image('wall', 'assets/backgrounds/tile.png');
 }
+
+var walls;
+var player;
+var gameWidth;
+var gameHeight;
 
 function create() {
 
-  var gameWidth = game.world.width;
-  var gameHeight = game.world.height;
-  game.add.sprite(0, 0, 'level1Background');
-  game.add.sprite(300, 200, 'player');
+  gameWidth = game.world.width;
+  gameHeight = game.world.height;
+  //game.add.sprite(0, 0, 'level1Background');
+  player = game.add.sprite(300, 200, 'player');
+  //game.add.sprite(300, 200, 'backgroundTile');
+
+  //walls = game.add.group();
+
+  //var wall1 = walls.create(20, 20, 'wall')
+  //wall1.scale.setTo(560, 80);
+
 
 }
 
+var xDir = 1;
+var yDir = -1;
+var xSpeed = 100;
+var ySpeed = 100;
+
 function update() {
+
+  player.y = player.y + yDir * ySpeed;
+  player.x = player.x + xDir * xSpeed;
+
+  if (player.x + player.width > gameWidth - 1) {
+    xDir = -1;
+  }
+
+  if (player.x < 0 + 1) {
+    xDir = 1;
+  }
+
+  if (player.y + player.height > gameHeight - 1) {
+    yDir = -1;
+  }
+
+  if (player.y < 0 + 1) {
+    yDir = 1;
+  }
 
 }
