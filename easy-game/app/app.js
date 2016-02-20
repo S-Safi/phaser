6b12bc1feee126ced3f0de407fa59edb36f17daf
Salam Almajdi <salamalmajdi@gmail.com>
2016-02-21 09:28:40 +1100

    Added physics and started tile map.

--- easy-game/app/app.js
+++ easy-game/app/app.js
@@ -7,22 +7,31 @@ var game = new Phaser.Game(
 );
 
 function preload() {
-  game.load.image('level1Background', 'assets/levels/test.png');
+  game.load.image('level1Background', 'assets/levels/test3.png');
   game.load.image('player', 'assets/player/derp-ssundee.png');
   game.load.image('wall', 'assets/backgrounds/tile.png');
 }
+
+var SPEED = 50;
 
 var walls;
 var player;
 var gameWidth;
 var gameHeight;
+var cursors;
 
 function create() {
 
   gameWidth = game.world.width;
   gameHeight = game.world.height;
-  //game.add.sprite(0, 0, 'level1Background');
+  background = game.add.sprite(200, 0, 'level1Background');
   player = game.add.sprite(300, 200, 'player');
+
+  game.physics.arcade.enable(player);
+  game.physics.arcade.enable(background);
+
+  player.body.collideWorldBounds = true;
+
   //game.add.sprite(300, 200, 'backgroundTile');
 
   //walls = game.add.group();
@@ -30,33 +39,58 @@ function create() {
   //var wall1 = walls.create(20, 20, 'wall')
   //wall1.scale.setTo(560, 80);
 
+  cursors = game.input.keyboard.createCursorKeys();
+
 
 }
 
-var xDir = 1;
-var yDir = -1;
-var xSpeed = 100;
-var ySpeed = 100;
+//var xDir = 1;
+// var yDir = -1;
+// var xSpeed = 2;
+// var ySpeed = 2;
 
 function update() {
 
-  player.y = player.y + yDir * ySpeed;
-  player.x = player.x + xDir * xSpeed;
-
-  if (player.x + player.width > gameWidth - 1) {
-    xDir = -1;
+  if (cursors.left.isDown) {
+    player.body.velocity.x = -SPEED;
+  }
+  else if (cursors.right.isDown) {
+    player.body.velocity.x = SPEED;
+  }
+  else {
+    player.body.velocity.x = 0;
   }
 
-  if (player.x < 0 + 1) {
-    xDir = 1;
+  if (cursors.up.isDown) {
+    player.body.velocity.y = -SPEED;
+  }
+  else if (cursors.down.isDown) {
+    player.body.velocity.y = SPEED;
+  }
+  else {
+    player.body.velocity.y = 0;
   }
 
-  if (player.y + player.height > gameHeight - 1) {
-    yDir = -1;
-  }
+  game.physics.arcade.collide(player, background);
 
-  if (player.y < 0 + 1) {
-    yDir = 1;
-  }
+
+  //player.y = player.y + yDir * ySpeed;
+  //player.x = player.x + xDir * xSpeed;
+
+  // if (player.x + player.width > gameWidth - 1) {
+  //   xDir = -1;
+  // }
+  //
+  // if (player.x < 0 + 1) {
+  //   xDir = 1;
+  // }
+  //
+  // if (player.y + player.height > gameHeight - 1) {
+  //   yDir = -1;
+  // }
+  //
+  // if (player.y < 0 + 1) {
+  //   yDir = 1;
+  // }
 
 }
